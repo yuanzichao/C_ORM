0f09f63048754c8d12eb11b13a856497d55aa8f1
chao <1162039386@qq.com>
2015-05-20 21:46:29 +0800

    reset

--- src/test_orm.c
+++ src/test_orm.c
@@ -40,34 +40,36 @@ int main(void) {
 
 	//the *__new method creates empty structure
 	db_disk_info *disk_info;
-	disk_info = db_disk_info__new();
+//	disk_info = db_disk_info__new();
+//
+//
+//	//获取系统当前时间并格式化
+//	time_t calendar_time = time(NULL);
+////	struct tm * tm_local = localtime(&calendar_time);
+////	char str_f_t [LEN];
+////	strftime(str_f_t, sizeof(str_f_t), "%G-%m-%d %H:%M:%S", tm_local);
+//
+//	//初始化Disk_Info
+//	disk_info->disk_name = "disk_003";
+//	disk_info->disk_uuid = "5a9068d5a7bf4b3bbec918af44e9d4e0";
+//	disk_info->disk_type = "ext3/ext4";
+//	disk_info->disk_capacity = 1024.00;
+//	disk_info->disk_used = 300.00;
+//	disk_info->recent_use_time = calendar_time;
+//	disk_info->disk_status = 0;
+//	disk_info->permisssion = 0;
+//
+//    /*
+//     * this method inserts the structure into according table.
+//     * If it has serial field, its value is reflected into structure
+//    */
+//    db_disk_info__insert(disk_info);
 
+	//Test query
+	disk_info = db_disk_info__get_by_id(3);
 
-	//获取系统当前时间并格式化
-	time_t calendar_time = time(NULL);
-//	struct tm * tm_local = localtime(&calendar_time);
-//	char str_f_t [LEN];
-//	strftime(str_f_t, sizeof(str_f_t), "%G-%m-%d %H:%M:%S", tm_local);
-
-	//初始化Disk_Info
-	disk_info->disk_name = "disk_003";
-	disk_info->disk_uuid = "5a9068d5a7bf4b3bbec918af44e9d4e0";
-	disk_info->disk_type = "ext3/ext4";
-	disk_info->disk_capacity = 1024.00;
-	disk_info->disk_used = 300.00;
-	disk_info->recent_use_time = calendar_time;
-	disk_info->disk_status = 0;
-	disk_info->permisssion = 0;
-
-    /*
-     * this method inserts the structure into according table.
-     * If it has serial field, its value is reflected into structure
-    */
-    db_disk_info__insert(disk_info);
+	printf("Disk Name=====>>>>%s\n", disk_info->disk_name);
 
 
     return EXIT_SUCCESS;
 }
-
-
-
